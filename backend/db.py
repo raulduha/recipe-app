56209dd80c7189a9437977b6ed8fa5b69c363961
jmcaussade <jmcaussade@miuandes.cl>
2024-11-14 00:19:15 -0300

    test conexión recipe_app con postgres server

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,8 @@ from databases import Database
 import sqlalchemy
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = "postgresql+asyncpg://postgres:your_password@localhost/recipe_app"
+DATABASE_URL = "postgresql+asyncpg://admin:admin1@localhost/recipe_app"
+
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
