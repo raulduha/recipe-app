b4837da0090641d63e785cca50e33958ad676a35
jmcaussade <jmcaussade@miuandes.cl>
2024-11-13 23:48:55 -0300

    schemas creados y database ul modificado

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ from databases import Database
 import sqlalchemy
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = "postgresql+asyncpg://your_username:your_password@localhost/CookingRecipes"
+DATABASE_URL = "postgresql+asyncpg://postgres:your_password@localhost/recipe_app"
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
